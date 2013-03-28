9cb556b3e0ef412ccf3bc640d02ecb828891c430
Dimitri Kurashvili <dimitri@c12.ge>
2013-03-28 20:53:38 +0400

    change some names

--- lib/forma/form/main.rb
+++ lib/forma/form/main.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
-require 'forma/form/main/column'
-require 'forma/form/main/tab'
+require 'forma/form/main/form_column'
+require 'forma/form/main/form_tab'
 require 'forma/form/main/form'
