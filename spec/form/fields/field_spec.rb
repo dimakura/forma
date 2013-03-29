a13d0c0d7779b38c1b84d2aed4f5d2c15ce4638b
Dimitri Kurashvili <dimitri@c12.ge>
2013-03-29 23:36:36 +0400

    remove old specs

--- spec/form/fields/field_spec.rb
+++ spec/form/fields/field_spec.rb
@@ -17,30 +17,30 @@ describe 'Field creation' do
   compare_object(Field.new(caption: 'Created', readonly: true), { caption: 'Created', before: nil, after: nil, readonly: true, required: false })
 end
 
-describe 'Field generation' do
-  before(:all) do
-    @f = Field.new(caption: 'Amount', before: '=', after: 'GEL')
-  end
-  context do
-    subject { @f }
-    its(:caption) { should == 'Amount' }
-  end
-  context do
-    subject { @f.cell_element }
-    its(:tag) { should == 'div'}
-    specify { subject[:class].should == [ 'ff-cell' ] }
-    specify { subject.children.size.should == 2 }
-  end
-  context do
-    subject { @f.cell_element.children[0] }
-    specify { subject.tag.should == 'span' }
-    specify { subject[:class].should == [ 'ff-before' ] }
-    specify { subject.text.should == '=' }
-  end
-  context do
-    subject { @f.cell_element.children[1] }
-    specify { subject.tag.should == 'span' }
-    specify { subject[:class].should == [ 'ff-after' ] }
-    specify { subject.text.should == 'GEL' }
-  end
-end
+# describe 'Field generation' do
+#   before(:all) do
+#     @f = NumberField.new(caption: 'Amount', before: '=', after: 'GEL')
+#   end
+#   context do
+#     subject { @f }
+#     its(:caption) { should == 'Amount' }
+#   end
+#   context do
+#     subject { @f.cell_element }
+#     its(:tag) { should == 'div'}
+#     specify { subject[:class].should == [ 'ff-cell' ] }
+#     specify { subject.children.size.should == 2 }
+#   end
+#   context do
+#     subject { @f.cell_element.children[0] }
+#     specify { subject.tag.should == 'span' }
+#     specify { subject[:class].should == [ 'ff-before' ] }
+#     specify { subject.text.should == '=' }
+#   end
+#   context do
+#     subject { @f.cell_element.children[1] }
+#     specify { subject.tag.should == 'span' }
+#     specify { subject[:class].should == [ 'ff-after' ] }
+#     specify { subject.text.should == 'GEL' }
+#   end
+# end
