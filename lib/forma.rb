cf54417e1f44a76fc773de01f6c8a2d7a4da2328
Dimitri Kurashvili <dimitri@c12.ge>
2013-03-29 12:13:57 +0400

    return version.rb to upper level

--- lib/forma.rb
+++ lib/forma.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'active_support/core_ext'
+require 'forma/version'
 require 'forma/utils'
 require 'forma/html'
 require 'forma/form'
--- lib/forma/utils.rb
+++ lib/forma/utils.rb
@@ -1,5 +1,4 @@
 # -*- encoding : utf-8 -*-
-require 'forma/utils/version'
 require 'forma/utils/engine' if defined?(Rails)
 require 'forma/utils/init'
 require 'forma/utils/config'
