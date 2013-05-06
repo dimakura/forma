eb84e5599ba19c854df4741f587106c2ef3f250d
Dimitri Kurashvili <dimitri@c12.ge>
2013-05-06 17:01:58 +0400

    initialize auth_token in forma helper

--- lib/forma/helpers.rb
+++ lib/forma/helpers.rb
@@ -5,6 +5,7 @@ module Forma
     def forma_for(model, opts = {}, &block)
       opts[:model] = model
       opts[:edit] = true
+      opts[:auth_token] = form_authenticity_token if defined?(Rails)
       f = Forma::Form.new(opts)
       yield f
       f.to_html.to_s
