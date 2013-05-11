69a907f5f4926f0f048da7c301773c906216681f
Dimitri Kurashvili <dimitri@c12.ge>
2013-05-11 23:49:17 +0400

    adding some fields into forma spec

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -4,12 +4,15 @@ include Forma::Helpers
 
 describe "forma helper" do
   before(:all) do
-    @user_password = { username: 'dimitri', password: 'secret' }
+    @user_password = { username: 'dimitri', password: 'secret', age: 33, email: 'dimakura@gmail.com' }
     @forma = forma_for(@user_password) do |f|
       f.text_field :username
       f.password_field :password
+      f.number_field :age
+      f.email_field :email
     end
-    #puts Nokogiri::XML(@forma, &:noblanks).to_xhtml(indent: 2)
+    # puts Nokogiri::XML(@forma, &:noblanks).to_xhtml(indent: 2)
   end
   specify { @forma.should_not be_nil }
+  specify { @forma.should be_a String }
 end
