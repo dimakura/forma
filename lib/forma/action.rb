216d8145e0724e7825318ed0961e972062938abe
Dimitri Kurashvili <dimakura@gmail.com>
2013-05-14 17:03:42 +0400

    adding item_actions to table

---
+++ lib/forma/action.rb
@@ -0,0 +1,40 @@
+module Forma
+  class Action
+    include Forma::Html
+    # attr_reader :label, :icon, :method, :confirm, :as
+    attr_reader :url 
+
+    def initialize(h={})
+      h = h.symbolize_keys
+      @label = h[:label]
+      @icon = h[:icon]
+      @url = h[:url]
+      @method = h[:method]
+      @confirm = h[:confirm]
+      @as = h[:as]
+    end
+
+    def to_html(model)
+      children = [ (el('img', attrs: { src: @icon }) if @icon.present?), el('span', text: eval_label(model)) ]
+      button = (@as.to_s == 'button')
+      el(
+        'a',
+        attrs: {
+          class: ['ff-action', ('btn' if button)],
+          href: eval_url(model), 'data-method' => @method, 'data-confirm' => @confirm
+        },
+        children: children
+      )
+    end
+
+    private
+
+    def eval_url(model)
+      @url.is_a?(Proc) ? @url.call(model) : @url.to_s
+    end
+
+    def eval_label(model)
+      @label.is_a?(Proc) ? @label.call(model) : @label.to_s
+    end
+  end
+end
--- lib/forma/table.rb
+++ lib/forma/table.rb
@@ -4,7 +4,8 @@ module Forma
     include Forma::Html
     include Forma::FieldHelper
     include Forma::WithTitleElement
-    attr_reader :collapsible, :collapsed, :icon, :title, :title_actions
+    attr_reader :collapsible, :collapsed, :icon, :title
+    attr_reader :title_actions
 
     def initialize(h = {})
       h = h.symbolize_keys
@@ -19,8 +20,7 @@ module Forma
       @fields = h[:fields] || []
       # actions
       @title_actions = h[:title_actions] || []
-      @top_actions = h[:top_actions] || []
-      @bottom_actions = h[:bottom_actions] || []
+      @item_actions = h[:item_actions] || []
     end
 
     def to_html
@@ -37,6 +37,11 @@ module Forma
     def title_action(url, h={})
       h[:url] = url
       @title_actions << Action.new(h)
+    end
+
+    def item_action(url, h={})
+      h[:url] = url
+      @item_actions << Action.new(h)
     end
 
     def add_field(f)
@@ -57,22 +62,30 @@ module Forma
 
     def table_element
       def table_header_element
+        children = @fields.map { |f|
+          label_text = f.label_i18n(@models.first)
+          label_hint = f.hint_i18n(@models.first)
+          el('th', attrs: { class: 'ff-field' }, text: label_text, children: [
+            (el('i', attrs: { class: 'ff-field-hint', 'data-toggle' => 'tooltip', title: label_hint }) if label_hint.present?)
+          ])
+        }
+        children << el('th', attrs: { style: {width: '100px'} }) if @item_actions.any?
         el('thead', children: [
-          el('tr', children: @fields.map { |f|
-            label_text = f.label_i18n(@models.first)
-            label_hint = f.hint_i18n(@models.first)
-            el('th', attrs: { class: 'ff-field' }, text: label_text, children: [
-              (el('i', attrs: { class: 'ff-field-hint', 'data-toggle' => 'tooltip', title: label_hint }) if label_hint.present?)
-            ])
-          })
+          el('tr', children: children)
         ])
       end
+      def table_row(model)
+        children = @fields.map { |fld|
+          el('td', children: [ fld.to_html(model, false) ])
+        }
+        if @item_actions.any?
+          children << el('td', children: @item_actions.map { |act| act.to_html(model) })
+        end
+        el('tr', children: children)
+      end
       def table_body_element
-        el('tbody', children: @models.map { |model|
-          el('tr', children: @fields.map { |fld|
-            el('td', children: [ fld.to_html(model, false) ])
-          })
-        })
+        children = 
+        el('tbody', children: @models.map { |model| table_row(model) })
       end
       if @models and @models.any?
         el('table', attrs: { class: 'ff-common-table' }, children: [ table_header_element, table_body_element ])
