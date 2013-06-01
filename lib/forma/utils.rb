5c2d308beca7bbfba7a08d4ac2193e7558a4f146
Dimitri Kurashvili <dimitri@c12.ge>
2013-06-01 14:59:51 +0400

    number formatting

--- lib/forma/utils.rb
+++ lib/forma/utils.rb
@@ -1,6 +1,10 @@
 # -*- encoding : utf-8 -*-
+require 'action_controller'
+
 module Forma
   module Utils
+    include ActionView::Helpers::NumberHelper
+
     def singular_name(model)
       if model.respond_to?(:model_name); model.model_name.singular_route_key # Mongoid
       elsif model.class.respond_to?(:table_name); model.class.table_name.singularize # ActiveModel
@@ -18,7 +22,25 @@ module Forma
       val
     end
 
+    def number_format(num, h = {})
+      max_digits = h[:max_digits] || 2
+      max_digits = 0 if max_digits < 0
+      min_digits = h[:min_digits] || 0
+      min_digits = max_digits if min_digits > max_digits
+      separator = h[:separator] || '.'
+      delimiter = h[:delimiter] || ','
+      formatted = number_with_precision(num, precision: max_digits, separator: separator, delimiter: delimiter, strip_insignificant_zeros: true)
+      nums = formatted.split(separator)
+      length_after_separator = (nums[1] || '').length
+      if length_after_separator >= min_digits
+        formatted
+      else
+        "#{nums[0]}#{separator}#{(nums[1] || '').ljust(min_digits, '0')}"
+      end
+    end
+
     module_function :singular_name
     module_function :extract_value
+    module_function :number_format
   end
 end
