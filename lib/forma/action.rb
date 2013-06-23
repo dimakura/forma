1207be594ca9e16b16e51413f6a80f854fdaf5c0
Dimitri Kurashvili <dimakura@gmail.com>
2013-06-23 20:23:22 +0400

    conditional actions

--- lib/forma/action.rb
+++ lib/forma/action.rb
@@ -16,32 +16,35 @@ module Forma
       @as = h[:as]
       @tooltip = h[:tooltip]
       @select = h[:select]
+      @condition = h[:condition]
     end
 
     def to_html(model)
-      if @select
-        el(
-          'a',
-          attrs: {
-            id: @id, class: ['ff-action', 'btn', 'btn-mini', 'ff-select-action'],
-            href: '#', 'data-original-title' => @tooltip,
-            'data-value-id' => model.id, 'data-value-text' => model.to_s
-          },
-          children: [ el('i', attrs: { class: 'icon icon-download' }) ]
-        )
-      else
-        children = [ (el('img', attrs: { src: @icon }) if @icon.present?), el('span', text: eval_label(model)) ]
-        button = (@as.to_s == 'button')
-        el(
-          'a',
-          attrs: {
-            id: @id,
-            class: ['ff-action', ('btn btn' if button)],
-            href: eval_url(model), 'data-method' => @method, 'data-confirm' => @confirm,
-            'data-original-title' => @tooltip
-          },
-          children: children
-        )
+      if eval_condition(model)
+        if @select
+          el(
+            'a',
+            attrs: {
+              id: @id, class: ['ff-action', 'btn', 'btn-mini', 'ff-select-action'],
+              href: '#', 'data-original-title' => @tooltip,
+              'data-value-id' => model.id, 'data-value-text' => model.to_s
+            },
+            children: [ el('i', attrs: { class: 'icon icon-download' }) ]
+          )
+        else
+          children = [ (el('img', attrs: { src: @icon }) if @icon.present?), el('span', text: eval_label(model)) ]
+          button = (@as.to_s == 'button')
+          el(
+            'a',
+            attrs: {
+              id: @id,
+              class: ['ff-action', ('btn btn' if button)],
+              href: eval_url(model), 'data-method' => @method, 'data-confirm' => @confirm,
+              'data-original-title' => @tooltip
+            },
+            children: children
+          )
+        end
       end
     end
 
@@ -53,6 +56,11 @@ module Forma
 
     def eval_label(model)
       @label.is_a?(Proc) ? @label.call(model) : @label.to_s
+    end
+
+    def eval_condition(model)
+      if @condition then @condition.is_a?(Proc) ? @condition.call(model) : @condition
+      else true end
     end
   end
 end
