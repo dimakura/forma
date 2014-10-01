d03415453808f98bb9a55dd5311dedc968c00cc9
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-01 19:59:57 +0400

    change field generator

--- lib/forma/generators/field_generator.rb
+++ lib/forma/generators/field_generator.rb
@@ -3,46 +3,40 @@ require 'forma/generators/html'
 
 module Forma
   module FieldGenerator
-    class ViewerGenerator
-      include Forma::Html
+    extend Forma::Html
 
-      def initialize(field)
-        @field = field
-        @type = @field.type || 'text'
-      end
-
-      def to_html(opts)
-        el('span', [ value_eval ], { class: class_name_eval })
-      end
-
-      private
-
-      def value_eval
-        if @field.value
-          @field.value
-        elsif @field.model and @field.name
-          if @field.model.instance_of?(Hash)
-            @field.model[ @field.name.to_sym ]
-          else
-            @field.model.send( @field.name.to_sym )
-          end
-        end
-      end
-
-      def class_name_eval
-        "forma-#{@type}-field"
-      end
-    end
-
-    def viewer_html(field, opts={})
-      ViewerGenerator.new(field).to_html(opts)
-    end
-
-    def editor_html(field, opts={})
-      '<p>editor</p>'
-    end
+    def viewer_html(field, opts); viewer(field, opts) end
+    def editor_html(field, opts); '<p>editor</p>' end
 
     module_function :viewer_html
     module_function :editor_html
+
+## Viewer creation functions
+
+    def viewer(field, opts)
+      model = opts[:model]
+      el('span', [ value_eval(field, model) ], { class: class_name_eval(field, model) })
+    end
+
+    def type_eval(field, model); field.type || 'text' end
+    def class_name_eval(field, model); "forma-#{type_eval(field, model)}-field" end
+
+    def value_eval(field, model)
+      if field.value
+        field.value
+      elsif (model || field.model) and field.name
+        evalModel = model || field.model
+        if evalModel.instance_of?(Hash)
+          evalModel[ field.name.to_sym ]
+        else
+          evalModel.send( field.name.to_sym )
+        end
+      end
+    end
+
+    module_function :viewer
+    module_function :value_eval
+    module_function :type_eval
+    module_function :class_name_eval
   end
 end
