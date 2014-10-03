7f6bc124e5b88ad4727b80e4d1727288b5bfcda6
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-03 17:44:17 +0400

    extend autoinit
    if we pass property value to the object like this:
    
    object.property value # without `=`!
    
    then property should be assigned

--- lib/forma/auto_initialize.rb
+++ lib/forma/auto_initialize.rb
@@ -11,6 +11,8 @@ module Forma
     def method_missing(method_name, *args, &block)
       if method_name.to_s[-1] == '='
         @opts[ method_name.to_s.chop.to_sym ] = args[0]
+      elsif args.length == 1
+        @opts[ method_name ] = args[0]
       else
         @opts[ method_name ]
       end
--- spec/autoinit_spec.rb
+++ spec/autoinit_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe Forma::AutoInitialize do
   before(:all) do
     @user = User.new(first_name: 'Dimitri', last_name: 'Kurashvili')
     @user.username = '555666777'
+    @user.number 42
   end
 
   specify{ expect(@user.first_name).to eq('Dimitri') }
   specify{ expect(@user.last_name).to eq('Kurashvili') }
   specify{ expect(@user.username).to eq('555666777') }
+  specify{ expect(@user.number).to eq(42) }
 end
