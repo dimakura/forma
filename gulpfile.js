47093a93bdb3c8193d4849c7bbb09badee06a022
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-03 20:11:15 +0400

    gulp's browserify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+  return browserify()
+    .require('./js/forma.js', { expose: 'forma' })
+    .bundle()
+    .pipe(source('./forma.js'))
+    .pipe(gulp.dest('./vendor/assets/javascripts'));
+});
 
 gulp.task('less', function () {
   gulp.src('./less/forma.less')
