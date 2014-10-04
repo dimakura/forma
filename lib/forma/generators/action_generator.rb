a720a4f1a1329b002cd5bd8e65ff5e910c5344d1
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-04 18:28:44 +0400

    adding confirmation to actions

--- lib/forma/generators/action_generator.rb
+++ lib/forma/generators/action_generator.rb
@@ -9,8 +9,11 @@ module Forma
     def to_html(actionDef, opts = {})
       url = opts[:url] || actionDef.url
       method = opts[:http_method] || actionDef.http_method
+      confirm = opts[:confirm] || actionDef.confirm
+
       params = { href: url }
       params['data-method'] = method if method.present?
+      params['data-confirm'] = confirm if confirm.present?
       params[:class] = 'forma-action'
 
       case (opts[:as] || actionDef.as).to_s
