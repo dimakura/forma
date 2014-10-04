9ae71683d08aa9ccace3387633bba5e7a8c1d614
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-04 19:11:16 +0400

    simple specs for table actions

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Forma::Table do
       User.new({ first_name: 'Dimitri', last_name: 'Kurashvili', age: 35 }),
       User.new({ first_name: 'Misho', last_name: 'Kurashvili', age: 8 }),
     ])
+    @table.action '/notify', http_method: 'post', label: 'Send Notifications'
     @html = @table.viewer_html
     @table2 = Forma::Table.new(fields: fields)
     @html2 = @table2.viewer_html
@@ -21,6 +22,9 @@ RSpec.describe Forma::Table do
   specify{ expect(@html).to include('<th width="200">First Name</th>') }
   specify{ expect(@html).to include('<th>Last Name</th>') }
   specify{ expect(@html).to include('<th>Age</th>') }
+
+  specify{ expect(@html).to include('Send Notifications') }
+  specify{ expect(@html).to include('href="/notify"') }
 
   specify{ expect(@html).to include('Dimitri') }
   specify{ expect(@html).to include('Kurashvili') }
