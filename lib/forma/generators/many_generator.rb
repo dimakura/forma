6b03ec52c79e8bcd0e05f31dc49cd047b09e1526
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-07 20:20:48 +0400

    generate editor with full attributes set from the "many" field

--- lib/forma/generators/many_generator.rb
+++ lib/forma/generators/many_generator.rb
@@ -7,13 +7,10 @@ require 'forma/generators/editor_generator'
 module Forma
   module ManyGeneratorMethods
     def editor_for_many_eval(field, values, opts)
-
       field_name = Forma::FieldGenerator.editor_field_name_eval(field, opts)
 
-      fields = field.fields
-      fields << Forma::Field.new(name: 'id', type: 'hidden')
-      editor = Forma::Editor.new(fields: fields)
-      
+      editor = Forma::Editor.new(field.options)
+      editor.add_field Forma::Field.new(name: 'id', type: 'hidden')
       editors_html = values.map do |value|
         newopts = opts.merge({ model: value, name_prefix: field_name })
         fields_html = Forma::EditorGenerator.editor_fields_eval(editor, newopts)
